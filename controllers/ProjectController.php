a8671d76fd39f2837cb5a9d53e3b328d090c4cf0
Rita.D <dolmatova.m.k@gmail.com>
2017-05-07 15:13:36 +0700

    change pagination

--- controllers/ProjectController.php
+++ controllers/ProjectController.php
@@ -8,27 +8,35 @@
 
 namespace app\controllers;
 
-use app\models\Project;
-use app\models\Task;
 use Yii;
-use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\models\Project;
 use yii\data\Pagination;
-use yii\data\ActiveDataProvider;
 
 class ProjectController extends Controller
 {
-
+	public function beforeAction($action)
+	{
+		if (parent::beforeAction($action)) {
+			if (!\Yii::$app->user->can('pm')) {
+				throw new \yii\web\ForbiddenHttpException('Доступ закрыт.');
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
     public function actionList()
     {
         $model = new Project();
-        $projects = Project::find()->where(['status' => 1, 'pm_id' => Yii::$app->user->identity->user_id]);
+        $projects = Project::find()->where(['pm_id' => Yii::$app->user->identity->user_id]);
         $pagination = new Pagination([
-            'defaultPageSize' => 1,
+            'defaultPageSize' => 10,
             'totalCount' => $projects->count(),
         ]);
 
@@ -51,76 +59,6 @@ class ProjectController extends Controller
             'model' => $model,
             'project' => $project,
         ]);
-    }
-
-    public function actionInfoproject()    {
-        $model = new Task();
-        $proj_id=Yii::$app->request->get('project_id');
-        $project = Project::findOne(['project_id' => $proj_id]);
-        $projectname=$project->name;
-
-        $dataProvider = new ActiveDataProvider([
-            'query' => Task::find()->where(['project_id' => $proj_id]),
-            'pagination' => [
-                'pageSize' => 10,
-            ],
-        ]);
-
-        return $this->render('infoproject', [
-            'model' => $model,
-            'dataProvider' =>$dataProvider,
-            'projectname' => $projectname,
-        ]);
-    }
-
-    public function actionCreatetask()
-    {
-        $model = new Task();
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['showtask', 'id' => $model->task_id]);
-        } else {
-            return $this->render('createtask', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    protected function findModel($id)
-    {
-        if (($model = Task::findOne($id)) !== null) {
-            return $model;
-        } else {
-            throw new NotFoundHttpException('The requested page does not exist.');
-        }
-    }
-
-    public function actionShowtask($id)
-    {
-        return $this->render('showtask', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-
-
-    public function actionUpdatetask($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['infoproject','project_id' =>$model->project_id]);
-        } else {
-            return $this->render('updatetask', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    public function actionDeletetask($id)
-    {
-        $model = $this->findModel($id);
-        $model->delete();
-        return $this->redirect(['infoproject','project_id' =>$model->project_id]);
     }
 
     public function actionIndex(){

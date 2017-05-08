b1fb2479e2f39d8848184a53a13ea8bf2231727d
ASavinov <amazing_noire@mail.ru>
2017-05-08 21:12:50 +0700

    Edited Task's elements.

--- views/project/gantt.php
+++ views/project/gantt.php
@@ -1,35 +1,45 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: A7ttim
- * Date: 06.05.2017
- * Time: 23:01
- */
-
-/* @var $this yii\web\View */
-/* @var $form yii\bootstrap\ActiveForm */
-/* @var $model app\models\ContactForm */
-/* @var $model app\models\Task */
-
-use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
+use yii\widgets\Pjax;
 use yii\widgets\LinkPager;
-use app\models\Task;
 
-$this->title = 'Gantt';
-$this->params['breadcrumbs'][] = $this->title;
+Pjax::begin();
 ?>
-    <script src="/js/dhtmlxgantt.js" type="text/javascript" charset="utf-8"></script>
-    <link rel="stylesheet" href="/css/dhtmlxgantt.css" type="text/css" media="screen" title="no title" charset="utf-8">
+<div id="gantt_here" style='width:1500px; height:400px;'></div>
+<?php
+//начало многосточной строки, можно использовать любые кавычки
+$script = <<< JS
 
-    <style type="text/css">
-        html, body{ height:100%; padding:0px; margin:0px; overflow: hidden;}
-    </style>
-    <body>
-    <div id="gantt_here" style="width:100%; height: auto; min-height: 200px;"></div>
-    <script type="text/javascript">
-        var tasks =  = eval('<?= $json ?>');
+var tasks = {
+  data:[
+     {id:1, text:"Проект 1", start_date:"07.05.2017", duration:14, executor:"Петров А.А."},
+     {id:2, text:"Задача 1", start_date:"07.05.2017", duration:3, parent:1, executor:"Иванов И.И."},
+     {id:3, text:"Задача 2", start_date:"07.05.2017", duration:2, parent:1},
+     {id:4, text:"Подзадача 1 задачи 2", start_date:"07.05.2017", duration:3, parent:3}
+   ]
+};
+gantt.config.lightbox.sections = [
+    {name:"description", height:40, map_to:"text", type:"textarea",focus:true},          
+    {name:"time", type:"duration", map_to:"auto"},
+    {name:"executor", height:55, type:"select", map_to:"executor", options:[ 
+        {key:"Петров А.А.", label: "Петров А.А."},                                               
+        {key:"Иванов И.И.", label: "Иванов И.И."},                                             
+        {key:"Сидоров П.А.", label: "Сидоров П.А."}                                                 
+     ]}
+];
+gantt.config.columns =  [
+    {name:"id", label:"#", width:"40px", align:"center"},
+    {name:"text",       tree:true, width:"250px" },
+    {name:"start_date", align: "center" },
+    {name:"duration", align: "center", width:"100px" },
+    {name:"executor",   label:"Исполнитель",   align: "center" },
+    {name:"add", align: "center" }
+];
+gantt.config.grid_width = 640;
+
         gantt.init("gantt_here");
         gantt.parse(tasks);
-    </script>
-    </body>
+JS;
+//маркер конца строки, обязательно сразу, без пробелов и табуляции
+$this->registerJs($script, yii\web\View::POS_READY);
+Pjax::end(); ?>
+

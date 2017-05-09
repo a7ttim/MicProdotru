28b12964daf74a2aab2f200af5db6ef3ae8786a0
Rita.D <dolmatova.m.k@gmail.com>
2017-05-09 08:32:39 +0700

    small fixes

--- controllers/TaskController.php
+++ controllers/TaskController.php
@@ -40,7 +40,7 @@ class TaskController extends Controller
         $tasks = Project::find()
             ->joinWith('tasks')
             ->rightJoin('user','user.user_id=task.user_id')
-            ->select(['task.task_id','project.name as project','task.name as task','task.description','task.start_date','task.plan_end_date','task.employment_percentage','user.name as user'])
+            ->select(['task.task_id','project.name as project','task.name as task','task.description','task.start_date','task.plan_end_date','task.workload','user.name as user'])
             ->where(['task.user_id' => Yii::$app->user->identity->user_id ,'task.status' => 3])
             ->asArray();
         $pagination = new Pagination([

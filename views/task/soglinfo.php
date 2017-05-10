a46ae3d6023ebd42cd02a3184b282344acf3cf49
ASavinov <amazing_noire@mail.ru>
2017-05-10 20:22:04 +0700

    many Task's fixes

--- views/task/soglinfo.php
+++ views/task/soglinfo.php
@@ -17,8 +17,7 @@ use yii\widgets\LinkPager;
 use yii\widgets\Pjax;
 use yii\helpers\Url;
 ?>
-<div style="margin-left: 50px"><h1>Задача №<?=$tasks['task_id'];?> "<?=$tasks['task'];?>"</h1></div>
-<div style="display: inline-block; font-size: 25px">Длительность <div class="btn btn-primary btn-sm" ><?=$tasks->plan_end_date-$tasks->start_date;?></div></div>
+<div style="margin-left: 50px"><h1>Задача №<?=$tasks->task_id ?> "<?=$tasks->name ?>"</h1></div>
 <div style="margin-top: 2%">
     <table class="detailed">
         <thead>
@@ -33,45 +32,45 @@ use yii\helpers\Url;
         <tbody>
                 <tr>
                     <td>Название</td>
-                    <td><?=$tasks['task'];?></td>
+                    <td><?=$tasks->name ?></td>
                 </tr>
                 <tr>
                     <td>Проект</td>
-                    <td><?=$tasks['project'];?></td>
+                    <td><?=$tasks->project->name ?></td>
                 </tr>
                 <tr>
                     <td>Начало</td>
-                    <td><?=date("d.m.Y",strtotime($tasks['start_date']));?></td>
+                    <td><?=date("d.m.Y",strtotime($tasks->start_date)) ?></td>
                 </tr>
                 <tr>
                     <td>Длительность</td>
-                    <td><?= $tasks['plan_duration'] ?></td>
+                    <td><?= $tasks->plan_duration ?></td>
                 </tr>
                 <tr>
                     <td>Исполнитель</td>
-                    <td><?=$tasks['user'];?></td>
+                    <td><?=$tasks->user->name ?></td>
                 </tr>
                 <tr>
                     <td>Загруженность</td>
-                    <td><?=$tasks['employment_percentage'];?></td>
+                    <td><?=$tasks->employment_percentage ?></td>
                 </tr>
                 <tr>
                     <td>Описание</td>
-                    <td style="display: block; max-height: 300px; overflow-y: auto; overflow-x: hidden;"><?= $tasks['description'] ?></td>
+                    <td style="display: block; max-height: 300px; overflow-y: auto; overflow-x: hidden;"><?= $tasks->description ?></td>
                 </tr>
                 <tr>
                     <td>Департамент</td>
-                    <td><?=$tasks['department'];?></td>
+                    <td><?=$tasks->department->department_name ?></td>
                 </tr>
-                <?php if (!is_null($previous_task_name->name))
+                <?php if (!is_null($previous_task->name))
                 {
                     ?>
                     <tr>
                         <td>Предыдущая задача</td>
                         <td>
                             <?= Html::a(
-                                $previous_task_name->name,
-                                Url::to(['task/soglinfo', 'task_id' => $previous_task_id->previous_task_id])
+                                $previous_task->name,
+                                Url::to(['task/soglinfo', 'task_id' => $tasks->previous_task_id])
                             );
                             ?>
                         </td>
